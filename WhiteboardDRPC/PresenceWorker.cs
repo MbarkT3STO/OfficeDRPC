ae5cdd9397bc41ab787f0a307b0e3bd331372fc6
M B A R K <mbark1236@gmail.com>
2024-03-31 16:35:40 +0000

    Feat: Add support for Microsoft Whiteboard

---
+++ WhiteboardDRPC/PresenceWorker.cs
@@ -0,0 +1,105 @@
+using System;
+using System.IO;
+using System.Threading;
+using MBDRPC.Core;
+using MBDRPC.Helpers;
+
+namespace AccessDRPC
+{
+	public class PresenceWorker
+    {
+        private Presence presence                  = new Presence();
+        private string   officeAppSubscriptionType = "Mirosoft Office";
+        private bool     isFirstRun                = true;
+        private DateTime startTime;
+        private string   processName;
+
+        public Timer Timer;
+
+
+        /// <summary>
+        /// Starts the presence
+        /// </summary>
+        public void Start()
+		{
+            Timer = new Timer(_ => CheckMicrosoftWhiteboard(), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+
+
+        private void CheckMicrosoftWhiteboard()
+        {
+            //processName = "Microsoft.WhiteboardApp_8wekyb3d8bbwe!App";       
+            processName = "MicrosoftWhiteboard";
+
+            var isPowerPointAppRunning = RunningAppChecker.IsAppRunning(processName);
+            if (isPowerPointAppRunning)
+            {
+                if (isFirstRun)
+                {
+                    officeAppSubscriptionType = GetOfficeVersion();
+
+                    presence.InitializePresence("1224032701879947407");
+
+                    presence.UpdateLargeImage("whiteboardlogo", "Microsoft Whiteboard");
+                    presence.UpdateSmallImage("officelogo2", GetOfficeVersion());
+
+                    presence.UpdateDetails(officeAppSubscriptionType);
+
+
+                    startTime  = RunningAppChecker.GetProcessStartTime(processName);
+                    isFirstRun = false;
+                }
+
+                UpdatePresence();
+            }
+            else
+            {
+                presence.ShutDown();
+                isFirstRun = true;
+            }
+        }
+
+
+        /// <summary>
+        /// Updates the presence
+        /// </summary>
+        private void UpdatePresence()
+        {
+            UpdatePresenceTime();
+            presence.UpdatePresence();
+        }
+
+        /// <summary>
+        /// Updates the presence time
+        /// </summary>
+        private void UpdatePresenceTime()
+        {
+            var elapsedTime = (DateTime.Now - startTime).ToString(@"hh\:mm\:ss");
+            presence.UpdateState(elapsedTime);
+        }
+
+
+        public static string GetOfficeVersion()
+        {
+            string appDataPath   = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string office365Path = Path.Combine(appDataPath, "Microsoft", "Office");
+
+            string programFilesPath    = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            string perpetualOfficePath = Path.Combine(programFilesPath, "Microsoft Office", "root", "Office16");
+
+            if (Directory.Exists(office365Path))
+            {
+                return "Microsoft 365";
+            }
+            else if (Directory.Exists(perpetualOfficePath))
+            {
+                return "Microsoft Office";
+            }
+            else
+            {
+                return "Microsoft Office";
+            }
+        }
+
+    }
+}
